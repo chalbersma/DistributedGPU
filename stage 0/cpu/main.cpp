5ea8227413f37ebceb4c8cbb733dc2c8e97067e8
Chris Halbersma <chalbersma.12@gmail.com>
2013-06-05 15:18:08 -0500

    starting cpu test
    nt.

---
+++ stage 0/cpu/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <time>
+#include <cmath>
+
+
+using namespace std;
+
+int main(){
+const unsigned long int rounds = 10000
+
+    for (int i = 0; i < 10000; i++){
+        // Assign random variables
+        // if statement for in circle
+            // in circle counter++
+
+    // PI calculation
+}
