e5120110c06535f76207448edbfaff1543cf4193
Chris Halbersma <chalbersma.12@gmail.com>
2013-06-05 16:25:26 -0500

    added basic c++ implementation
    nt

--- stage 0/cpu/main.cpp
+++ stage 0/cpu/main.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
-#include <time>
+#include <time.h>
 #include <cmath>
+#include <random>
+#include <chrono>
 
-
+const unsigned long int rounds = 10000;
 using namespace std;
 
 int main(){
-const unsigned long int rounds = 10000
 
-    for (int i = 0; i < 10000; i++){
+    unsigned long int total = 0;
+    unsigned long int incircle = 0;
+
+
+    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    default_random_engine generator (seed);
+    uniform_real_distribution<double> distribution(0.0,1.0);
+
+    for (unsigned int i = 0; i < rounds; i++){
+        total++;
         // Assign random variables
-        // if statement for in circle
-            // in circle counter++
+        double disx = distribution(generator);
+        double disy = distribution(generator);
 
+        cout << "( " << disx << " , " << disy << " ) " << endl;
+        if(disx*disx + disy*disy <= 1.0 )
+            incircle++;
+    }
     // PI calculation
+    cout << "In Cirlce: " << incircle << endl << "Total: " << total << endl;
+
+
+    double estpi = ( double(incircle) * double(4.0) ) / double(total);
+
+    cout << "Estimation of PI: " << estpi << endl;
+
+    return 0;
 }
